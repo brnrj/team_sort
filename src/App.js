374638b394f05cae5bdf9476a62a7493f3568c3e
Bruno Martins <bruno.martins217@gmail.com>
2021-04-29 01:59:21 -0300

    Versao Beta 1.0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { CSVReader } from 'react-papaparse';
+import logo from './images/logo.jpeg';
+import { readRemoteFile } from 'react-papaparse';
 
 function CSVReader2() {
+  const [overall] = useState(5)
+  const [name] = useState(0)
   const [data, setData] = useState([]);
   const [sortedData, setSortedData] = useState([]);
   const [checked, setChecked] = useState(false);
@@ -10,13 +14,23 @@ function CSVReader2() {
   const [team2, setTeam2] = useState([]);
   const [team3, setTeam3] = useState([]);
 
+  function handleClickWeb() {
+    readRemoteFile('https://docs.google.com/spreadsheets/d/e/2PACX-1vRoADoJc-w_dAjQ_GNlSX99KpDsdzYJ6Kau3OI50_f2CamLySulbEStphsrrvtiYns0Mk8aVlLyNSXl/pub?gid=238086313&single=true&output=csv', {
+      complete: (results) => {
+        console.log('Results:', results);
+        let teste = results.data.map((element) => ({...element}))
+        setData(teste)
+      },
+    });
+  };
+
   function handleClick() {
     setChecked(true);
   }
 
   function handleOnDrop(data) {
     console.log(data);
-    setData(data);
+    setData(data.map((element) => element.data));
   }
 
   function handleOnError(err) {
@@ -27,31 +41,54 @@ function CSVReader2() {
     console.log(data);
   }
 
-  function sortTopPlayer(data) {
-    data.sort((a, b) =>
-      a.data[5] > b.data[5] ? 1 : b.data[5] > a.data[5] ? -1 : 0
-    );
-    setSortedData(data);
-  }
-
-  useEffect(() => {
+  function algoritmoGuloso(sortedData){
     let time1 = [];
     let time2 = [];
     let time3 = [];
-    sortTopPlayer(data);
-    for (let i = 0; i < 24; i++) {
-      time1.push(sortedData[i]);
-      time2.push(sortedData[i + 1]);
-      time3.push(sortedData[i + 2]);
-      i = i + 2;
+    const obj = {
+      jogador : sortedData
+    }
+
+    // let times = [time1, time2, time3]
+
+    // for(let i = 0; i < 24 ; i++){
+    //   times[i % 3].push(sortedData[i])
+    // }
+
+    let k = 0
+    for(let i = 0; i < 24 ; i += 3){
+      if (k % 2 === 0){
+        time1.push(obj.jogador[i])
+        time2.push(obj.jogador[i + 1])
+        time3.push(obj.jogador[i + 2])
+      }
+      else {
+        time1.push(obj.jogador[i + 2])
+        time2.push(obj.jogador[i + 1])
+        time3.push(obj.jogador[i])
+      }
+      k++
     }
     setTeam1(time1);
     setTeam2(time2);
     setTeam3(time3);
+  }
+
+  useEffect(() => {
+  sortTopPlayer(data);
+  algoritmoGuloso(sortedData);
   }, [data, sortedData]);
-  console.log(team1, team2, team3);
+
+  function sortTopPlayer(data) {
+      data.sort((a, b) =>
+      a[overall] > b[overall] ? 1 : b[overall] > a[overall] ? -1 : 0
+    );
+    setSortedData(data);
+  }
+  console.log(team1, team2, team3)
   return (
     <>
+      <button onClick={handleClickWeb}>readRemoteFile</button>
       <CSVReader
         onDrop={handleOnDrop}
         onError={handleOnError}
@@ -62,27 +99,32 @@ function CSVReader2() {
       </CSVReader>
       <div className="App">
         <div className='Meme'>
-          {!checked && <p>Vai um Gundanzim ai Fei?</p>}
+          {!checked && 
+          <div>
+            <img src={logo} alt="logo"/>
+            <p>Vai um Gundanzim ai Fei?</p>
+          </div>}
+          {checked && <p>MEME</p>}
           <button onClick={handleClick}>Clica Aqui Bonitão</button>
         </div> 
-        {checked && (
+        {(checked && data.length) && (
           <div className="Main-Team">
             <div className="Team">
-              <p>Time 1</p>
-              {team1.map(({data}, index) => (
-                <p className="LoL" key={index}>{data[0]}</p>
+              <p className="titulo">Time 1</p>
+              {team1.map((element, index) => (
+                <p className="LoL blue" key={index}>{element[name]}</p>
               ))}
             </div>
             <div className="Team">
-            <p>Time 2</p>
-            {team2.map(({data}, index) => (
-                <p className="LoL" key={index}>{data[0]}</p>
+            <p className="titulo">Time 2</p>
+            {team2.map((element, index) => (
+                <p className="LoL red" key={index}>{element[name]}</p>
               ))}
             </div>
             <div className="Team">
-            <p>Time 3</p>
-            {team3.map(({data}, index) => (
-                <p className="LoL" key={index}>{data[0]}</p>
+            <p className="titulo">Time 3</p>
+            {team3.map((element, index) => (
+                <p className="LoL green" key={index}>{element[name]}</p>
               ))}
             </div>
           </div>
